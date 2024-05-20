cc93c158b95e60794de85f580fb002724e46e2dd
DERK@RIOM <derkariom@gmail.com>
2024-05-20 13:43:14 +0000

    feat (*) : integration d' un lecteur PDF

--- fabi/src/main/java/com/ninotech/fabi/controleur/activity/InfosActivity.java
+++ fabi/src/main/java/com/ninotech/fabi/controleur/activity/InfosActivity.java
@@ -1,6 +1,7 @@
 package com.ninotech.fabi.controleur.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,6 +9,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.ninotech.fabi.R;
+import com.pspdfkit.configuration.activity.PdfActivityConfiguration;
+import com.pspdfkit.ui.PdfActivity;
 
 public class InfosActivity extends AppCompatActivity {
     private Button mButton;
@@ -18,6 +21,9 @@ public class InfosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_infos);
         getSupportActionBar().hide();
         mButton = findViewById(R.id.licence);
+        Uri uri = Uri.parse("file:///android_asset/sample.pdf");
+        PdfActivityConfiguration config = new PdfActivityConfiguration.Builder(this).build();
+        PdfActivity.showDocument(this,uri,config);
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
