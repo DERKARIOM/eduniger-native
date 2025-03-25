286f3d9864bb07ea5ed6b58155de2cc90437a007
DERK@RIOM <derkariom@gmail.com>
2025-03-25 13:48:47 +0100

    fix : 25/03/2025

---
+++ fabi/src/main/java/com/ninotech/fabi/controleur/adapter/SemiNoConnectionAdapter.java
@@ -0,0 +1,90 @@
+package com.ninotech.fabi.controleur.adapter;
+import android.content.Intent;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.ninotech.fabi.R;
+import com.ninotech.fabi.model.data.Connection;
+
+import java.util.List;
+
+public class SemiNoConnectionAdapter extends RecyclerView.Adapter<SemiNoConnectionAdapter.MyViewHolder> {
+    List<Connection> mListConnection;
+
+    public int getPosition() {
+        return mPosition;
+    }
+
+    public void setPosition(int position) {
+        mPosition = position;
+    }
+
+    private int mPosition;
+    public SemiNoConnectionAdapter(List<Connection> listConnection) {
+        mListConnection = listConnection;
+    }
+    @Override
+    public SemiNoConnectionAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.adapter_semi_no_connection,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Connection item = mListConnection.get(position);
+        int i = position;
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                mPosition = holder.getAdapterPosition();
+                view.showContextMenu();
+                return true;
+            }
+        });
+        holder.display(mListConnection.get(position));
+
+    }
+    @Override
+    public int getItemCount() {
+        return mListConnection.size();
+    }
+
+    public Connection getItem(int position) {
+        return mListConnection.get(position);
+    }
+
+    public void Remove(int position){
+        mListConnection.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener{
+       private final Button mButton;
+        MyViewHolder(View itemView){
+            super(itemView);
+           mButton = itemView.findViewById(R.id.button_adapter_semi_no_connection);
+            itemView.setOnCreateContextMenuListener(this);
+        }
+        @Override
+        public void onCreateContextMenu(ContextMenu menu , View v , ContextMenu.ContextMenuInfo menuInfo){
+        }
+        void display(Connection connection){
+            mButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(connection.getSource());
+                    itemView.getContext().sendBroadcast(intent);
+                }
+            });
+        }
+    }
+}
