cc24dd39746c83a851b69b585a52ca1756832b23
DERK@RIOM <derkariom@gmail.com>
2025-03-30 23:12:50 +0100

    feat : multi server

--- fabi/src/main/java/com/ninotech/fabi/controleur/activity/ServerActivity.java
+++ fabi/src/main/java/com/ninotech/fabi/controleur/activity/ServerActivity.java
@@ -1,92 +1,77 @@
 package com.ninotech.fabi.controleur.activity;
 
+import android.database.sqlite.SQLiteDatabase;
+import android.hardware.fingerprint.FingerprintManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.ninotech.fabi.R;
 import com.ninotech.fabi.controleur.adapter.StatusBarAdapter;
+import com.ninotech.fabi.model.data.Server;
+import com.ninotech.fabi.model.table.DigitalPrintTable;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.CancellationSignal;
+import android.os.Handler;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.annotation.Nullable;
 
 import java.util.Locale;
+import java.util.Objects;
 
 public class ServerActivity extends AppCompatActivity {
-    private Spinner spinnerLanguage;
-    private String[] languages = {"Français", "English", "العربية"};
-    private String[] languageCodes = {"fr", "en", "ar"};
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
        // loadLocale();
         setContentView(R.layout.activity_server);
         StatusBarAdapter statusBarAdapter = new StatusBarAdapter(this,getWindow());
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // Charger la langue sauvegardée
-
-        spinnerLanguage = findViewById(R.id.spinner_language);
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, languages);
-        spinnerLanguage.setAdapter(adapter);
-
-        // Sélectionner la langue actuelle
-        String currentLanguage = getSharedPreferences("Settings", MODE_PRIVATE).getString("My_Lang", "fr");
-        int index = getIndexForLanguage(currentLanguage);
-        spinnerLanguage.setSelection(index);
-
-        // Changer la langue lors de la sélection
-        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        mSwitch = findViewById(R.id.switch_activity_server);
+        mRadioGroup = findViewById(R.id.radio_group_activity_server);
+        switch (Server.getIpServer(getApplicationContext()))
+        {
+            case "https://telesafe.net/fabi/":
+                RadioButton radioButton1 = (RadioButton) mRadioGroup.getChildAt(1);
+                radioButton1.setChecked(true);
+                break;
+            case "http://192.168.1.25:2222/fabi/":
+                RadioButton radioButton2 = (RadioButton) mRadioGroup.getChildAt(2);
+                radioButton2.setChecked(true);
+                break;
+            default:
+                RadioButton radioButton3 = (RadioButton) mRadioGroup.getChildAt(3);
+                radioButton3.setChecked(true);
+                break;
+        }
+        mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                setLocale(languageCodes[position]);
-            }
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton selectedRadioButton = findViewById(checkedId);
+                int position =  group.indexOfChild(selectedRadioButton);
+                switch (position)
+                {
+                    case 1:
+                        Server.saveServer(getApplicationContext(),"https://telesafe.net/fabi/","https://telesafe.net/fabi/android/");
+                    break;
+                    case 2:
+                        Server.saveServer(getApplicationContext(),"http://192.168.1.25:2222/fabi/","http://192.168.1.25:2222/fabi/android/");
+                        break;
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+                }
             }
         });
+
     }
-
-    private int getIndexForLanguage(String langCode) {
-        for (int i = 0; i < languageCodes.length; i++) {
-            if (languageCodes[i].equals(langCode)) {
-                return i;
-            }
-        }
-        return 0; // Par défaut "français"
-    }
-
-    private void setLocale(String langCode) {
-        Locale locale = new Locale(langCode);
-        Locale.setDefault(locale);
-        Resources resources = getResources();
-        Configuration config = new Configuration(resources.getConfiguration());
-        config.setLocale(locale);
-        resources.updateConfiguration(config, resources.getDisplayMetrics());
-
-        // Sauvegarder la langue sélectionnée
-        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
-        editor.putString("My_Lang", langCode);
-        editor.apply();
-
-        // Redémarrer l'activité pour appliquer les changements
-        Intent intent = getIntent();
-        finish();
-        startActivity(intent);
-    }
-
-    private void loadLocale() {
-        SharedPreferences prefs = getSharedPreferences("Settings", MODE_PRIVATE);
-        String language = prefs.getString("My_Lang", "fr");
-        setLocale(language);
-    }
+    private Switch mSwitch;
+    private RadioGroup mRadioGroup;
 }
