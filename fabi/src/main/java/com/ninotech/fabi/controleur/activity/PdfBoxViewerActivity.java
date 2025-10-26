30880a31bfa7974e0011b7efd98df49e43a99047
DERK@RIOM <derkariom@gmail.com>
2025-10-26 16:08:54 +0100

    remplacement de PSPDFKit : version plus optimiser

--- fabi/src/main/java/com/ninotech/fabi/controleur/activity/PdfBoxViewerActivity.java
+++ fabi/src/main/java/com/ninotech/fabi/controleur/activity/PdfBoxViewerActivity.java
@@ -60,12 +60,12 @@ public class PdfBoxViewerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //getSupportActionBar().hide();
         // Mode plein écran pour format A5
-        getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
+//        getWindow().setFlags(
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN
+//        );
 
         setContentView(R.layout.activity_pdfbox_viewer);
 
