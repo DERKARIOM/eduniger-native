9d707ef2423e3d7e9feb14d93a13a0a8d89614fd
DERKARIOM <derkarom@gmail.com>
2026-02-05 15:54:24 +0100

    fix(*) : Adaptation du nom de project de RAS a EduNiger et de fabi a eduniger_native

---
+++ eduniger/build/generated/source/buildConfig/debug/com/ninotech/eduniger/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ninotech.eduniger;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ninotech.eduniger";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 13;
+  public static final String VERSION_NAME = "3.1.3";
+}
